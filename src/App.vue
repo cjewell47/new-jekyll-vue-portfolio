814020e77eb54d2d1b5e0199118a0acce3dafa16
cjewell47 <charles.jewell47@gmail.com>
2019-01-20 18:17:28 +0000

    completed header component

--- src/App.vue
+++ src/App.vue
@@ -6,24 +6,56 @@
 </template>
 
 <script>
-
-import header from './components/Header.vue';
+import header from "./components/Header.vue";
 export default {
   components: {
-    'app-header': header
-    
+    "app-header": header
   },
-  data () {
-    return {
-    }
+  data() {
+    return {};
   },
-  methods: {
-  }
-}
+  methods: {}
+};
 </script>
 
 <style lang="scss">
-  body {
-    margin: 0;
+body {
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  font-family: "futura", sans-serif;
+  font-style: italic;
+  font-weight: 300;
+  background: #f7797d;
+  background: -webkit-linear-gradient(to right, #f7797d, #fbd786, #c6ffdd);
+  background: linear-gradient(to right, #f7797d, #fbd786, #c6ffdd);
+  background-size: 300% 300%;
+  -webkit-animation: Gradient 10s ease infinite;
+  -moz-animation: Gradient 10s ease infinite;
+  animation: Gradient 10s ease infinite;
+}
+
+@-webkit-keyframes Gradient {
+  0% {
+    background-position: 0% 50%;
   }
+  50% {
+    background-position: 100% 50%;
+  }
+  100% {
+    background-position: 0% 50%;
+  }
+}
+
+@keyframes Gradient {
+  0% {
+    background-position: 0% 50%;
+  }
+  50% {
+    background-position: 100% 50%;
+  }
+  100% {
+    background-position: 0% 50%;
+  }
+}
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,18 +1,36 @@
 <template>
-  <div class="items-container">
-    <router-link to="/" exact><h1 data-section="Charles Jewell">Charles Jewell</h1></router-link>
-    <div class="header-items-container">
+  <div class="items-wrap">
+    <div class="items-container">
+      <router-link to="/" exact>
+        <h1 data-section="Charles Jewell">Charles Jewell</h1>
+      </router-link>
       <li>
-        <router-link to="/about" exact>About</router-link>
+        <router-link to="/about" exact>
+          <div class="header-item">
+            <p>About</p>
+          </div>
+        </router-link>
       </li>
       <li>
-        <router-link to="/skills" exact>Skills</router-link>
+        <router-link to="/skills" exact>
+          <div class="header-item">
+            <p>Skills</p>
+          </div>
+        </router-link>
       </li>
       <li>
-        <router-link to="/projects" exact>Projects</router-link>
+        <router-link to="/projects" exact>
+          <div class="header-item">
+            <p>Projects</p>
+          </div>
+        </router-link>
       </li>
       <li>
-        <router-link to="/contact" exact>Contact</router-link>
+        <router-link to="/contact" exact>
+          <div class="header-item">
+            <p>Contact</p>
+          </div>
+        </router-link>
       </li>
     </div>
   </div>
@@ -25,9 +43,135 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-$gray: darkslategrey;
-h1 {
-  color: $gray;
-  font-size: 2rem;
+a {
+  text-decoration: none;
+}
+.items-wrap {
+  background: #fff;
+  .items-container {
+    display: flex;
+    max-width: 900px;
+    margin: auto;
+    justify-content: space-evenly;
+    align-items: center;
+    h1 {
+      color: #000;
+      font-size: 48px;
+      margin: 0;
+    }
+    li {
+      list-style: none;
+    }
+    .router-link-active {
+      .header-item {
+        &:hover {
+          animation-name: headerActive;
+          animation-duration: 0.25s;
+          -webkit-animation-name: headerActive;
+          -webkit-animation-duration: 0.25s;
+          -moz-animation-name: headerActive;
+          -moz-animation-duration: 0.25s;
+        }
+        &::before {
+          transform: rotate(45deg);
+        }
+        &::after {
+          transform: rotate(135deg);
+        }
+        p {
+          opacity: 0;
+          transform: rotate(45deg);
+        }
+      }
+    }
+    .header-item {
+      opacity: 1;
+      position: relative;
+      height: 100px;
+      width: 100px;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      margin: 10px 0;
+      transition: all 0.75s ease-in-out;
+      -moz-transition: all 0.75s ease-in-out;
+      -webkit-transition: all 0.75s ease-in-out;
+      &:hover {
+        cursor: pointer;
+      }
+      &::before,
+      &::after {
+        position: absolute;
+        content: "";
+        width: 100%;
+        height: 20px;
+        background-color: #000;
+        transform: rotate(90deg);
+        transition: transform 0.45s ease-in-out;
+        -moz-transition: transform 0.45s ease-in-out;
+        -webkit-transition: transform 0.45s ease-in-out;
+      }
+      &:hover::before,
+      &.current::before {
+        transform: rotate(45deg);
+      }
+      &:hover::after,
+      &.current::after {
+        transform: rotate(135deg);
+      }
+      p {
+        transform: rotate(90deg);
+        color: #fff;
+        font-style: italic;
+        text-align: center;
+        z-index: 100;
+        letter-spacing: 1px;
+        transition: all 0.45s ease-in-out;
+        -moz-transition: all 0.45s ease-in-out;
+        -webkit-transition: all 0.45s ease-in-out;
+      }
+      &:hover p,
+      &.current p {
+        opacity: 0;
+        transform: rotate(45deg);
+      }
+    }
+  }
+}
+
+@keyframes headerActive {
+  0% {
+    transform: rotate(0deg);
+  }
+  25% {
+    transform: rotate(3deg);
+  }
+  50% {
+    transform: rotate(6deg);
+  }
+  75% {
+    transform: rotate(3deg);
+  }
+  100% {
+    transform: rotate(0deg);
+  }
+}
+
+@-webkit-keyframes headerActive {
+  0% {
+    transform: rotate(0deg);
+  }
+  25% {
+    transform: rotate(3deg);
+  }
+  50% {
+    transform: rotate(6deg);
+  }
+  75% {
+    transform: rotate(3deg);
+  }
+  100% {
+    transform: rotate(0deg);
+  }
 }
 </style>
