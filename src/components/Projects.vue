e1de668096a12b1d1a83f04aa3df1e3c55453a8c
cjewell47 <charles.jewell47@gmail.com>
2019-03-04 20:04:29 +0000

    cleaned up css, removed unnecessary classes

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -109,71 +109,14 @@
           </template>
         </about-project>
       </transition>
-      <!-- <div v-for="(project, index) in projects" v-bind:id="project.code" v-bind:key="index" class="image-box">
-        <img class="img-portfolio img-1" v-bind:src="project.img1">
-        <img class="img-portfolio img-2-1" v-bind:src="project.img2">
-        <div class="project-link">
-          <p>
-            <a v-bind:href="project.link" target="_blank">Visit</a> |
-            <span @click="() => openAbout(project.code)">Read more</span>
-          </p>
-        </div>
-      </div> -->
-      <project-image v-for="(project, index) in projects" v-bind:project="project" v-bind:id="project.code" v-bind:key="index" @read-more="($event) => openAbout($event)"></project-image>
-
-      <!-- <div class="image-box" v-bind:class="{'highlighted': aboutProject.bl}">
-        <img class="img-portfolio img-1" src="img/BlendLifeBW-min.png">
-        <img class="img-portfolio img-2-1" src="img/BlendLife-min.png">
-        <div class="project-link">
-          <p>
-            <a href="https://intense-dusk-18560.herokuapp.com/" target="_blank">Visit</a> |
-            <span @click="() => openAbout('bl')">Read more</span>
-          </p>
-        </div>
-      </div>
-      <div class="image-box" v-bind:class="{'highlighted': aboutProject.ffp}">
-        <img class="img-portfolio img-1" src="img/ffpbw.png">
-        <img class="img-portfolio img-2-1" src="img/ffp.png">
-        <div class="project-link">
-          <p>
-            <a
-              href="https://www.experian.co.uk/consumer/financial-fitness-plan/"
-              target="_blank"
-            >Visit</a> |
-            <span @click="() => openAbout('ffp')">Read more</span>
-          </p>
-        </div>
-      </div>
-      <div class="image-box" v-bind:class="{'highlighted': aboutProject.pp}">
-        <img class="img-portfolio img-1" src="img/PawsAndPlayBW-min.png">
-        <img class="img-portfolio img-2-2" src="img/PawsAndPlay-min.png">
-        <div class="project-link">
-          <p>
-            <a href="https://dogwalkingapp.herokuapp.com/" target="_blank">Visit</a> |
-            <span @click="() => openAbout('pp')">Read more</span>
-          </p>
-        </div>
-      </div>
-      <div class="image-box" v-bind:class="{'highlighted': aboutProject.sn}">
-        <img class="img-portfolio img-1" src="img/SnakeBW-min.png">
-        <img class="img-portfolio img-2-3" src="img/Snake-min.png">
-        <div class="project-link">
-          <p>
-            <a href="https://stormy-forest-65305.herokuapp.com/" target="_blank">Visit</a> |
-            <span @click="() => openAbout('sn')">Read more</span>
-          </p>
-        </div>
-      </div>
-      <div class="image-box" v-bind:class="{'highlighted': aboutProject.br}">
-        <img class="img-portfolio img-1" src="img/BeerRaterBW-min.png">
-        <img class="img-portfolio img-2-4" src="img/BeerRater-min.png">
-        <div class="project-link">
-          <p>
-            <a href="https://evening-refuge-83030.herokuapp.com/" target="_blank">Visit</a> |
-            <span @click="() => openAbout('br')">Read more</span>
-          </p>
-        </div>
-      </div> -->
+      <project-image 
+        v-for="(project, index) in projects" 
+        :project="project" 
+        :highlighted="aboutProject[project.code]" 
+        :id="project.code" 
+        :key="index + 1" 
+        @read-more="($event) => openAbout($event)">
+      </project-image>
     </div>
   </div>
 </template>
@@ -259,75 +202,6 @@ h1 {
   display: flex;
   flex-wrap: wrap;
   position: relative;
-  // .image-box {
-  //   position: relative;
-  //   height: 250px;
-  //   width: 250px;
-  //   display: flex;
-  //   align-items: center;
-  //   justify-content: center;
-  //   overflow: hidden;
-  //   object-fit: cover;
-  //   .img-portfolio {
-  //     height: 250px;
-  //     position: absolute;
-  //     transition: opacity 0.5s ease-in-out;
-  //     -webkit-transition: opacity 0.5s ease-in-out;
-  //     -moz-transition: opacity 0.5s ease-in-out;
-  //     &[class*="img-2-"] {
-  //       opacity: 0;
-  //       &:hover ~ .project-link {
-  //         opacity: 1;
-  //         top: 150px;
-  //       }
-  //     }
-  //   }
-  //   &:hover > .img-portfolio[class*="img-2-"] {
-  //     opacity: 1;
-  //   }
-  //   .project-link {
-  //     width: 200px;
-  //     background-color: #000;
-  //     color: #fff;
-  //     position: absolute;
-  //     text-align: center;
-  //     z-index: 2;
-  //     top: 235px;
-  //     opacity: 0;
-  //     transition: all 0.5s ease-in-out;
-  //     -webkit-transition: all 0.5s ease-in-out;
-  //     -moz-transition: all 0.5s ease-in-out;
-  //     p {
-  //       margin-top: 12px auto;
-  //       font-weight: 300;
-  //       font-size: 16px;
-  //     }
-  //     a,
-  //     span {
-  //       color: #fff;
-  //       text-decoration: none;
-  //       letter-spacing: 1px;
-  //       cursor: pointer;
-  //       &:hover {
-  //         text-decoration: underline;
-  //       }
-  //     }
-  //     &:hover {
-  //       opacity: 1;
-  //       top: 150px;
-  //     }
-  //   }
-  //   &.highlighted {
-  //     z-index: 4;
-  //     .img-portfolio[class*="img-2-"] {
-  //       opacity: 1;
-  //       &:hover ~ .project-link {
-  //         opacity: 0;
-  //         top: 235px;
-  //       }
-  //     }
-  //   }
-  // }
   @media screen and (max-width: 767px) {
     width: 250px;
     height: 1250px;
--- src/components/project-components/ProjectImage.vue
+++ src/components/project-components/ProjectImage.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="image-box">
-    <img class="img-portfolio img-1" v-bind:src="project.img1">
-    <img class="img-portfolio img-2-1" v-bind:src="project.img2">
-    <div class="project-link">
+  <div class="image-box" :class="{ 'highlighted': highlighted }">
+    <img :src="project.img1">
+    <img :src="project.img2">
+    <div>
       <p>
-        <a v-bind:href="project.link" target="_blank">Visit</a> |
+        <a :href="project.link" target="_blank">Visit</a> |
         <span @click="$emit('read-more', project.code)">Read more</span>
       </p>
     </div>
@@ -18,6 +18,10 @@ export default {
     project: {
       type: Object,
       required: true
+    },
+    highlighted: {
+      type: Boolean,
+      required: true
     }
   },
   data() {
@@ -27,73 +31,73 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-     .image-box {
-    position: relative;
+.image-box {
+  position: relative;
+  height: 250px;
+  width: 250px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  overflow: hidden;
+  object-fit: cover;
+  img {
     height: 250px;
-    width: 250px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    overflow: hidden;
-    object-fit: cover;
-    .img-portfolio {
-      height: 250px;
-      position: absolute;
-      transition: opacity 0.5s ease-in-out;
-      -webkit-transition: opacity 0.5s ease-in-out;
-      -moz-transition: opacity 0.5s ease-in-out;
-      &[class*="img-2-"] {
-        opacity: 0;
-        &:hover ~ .project-link {
-          opacity: 1;
-          top: 150px;
-        }
-      }
-    }
-    &:hover > .img-portfolio[class*="img-2-"] {
-      opacity: 1;
-    }
-    .project-link {
-      width: 200px;
-      background-color: #000;
-      color: #fff;
-      position: absolute;
-      text-align: center;
-      z-index: 2;
-      top: 235px;
+    position: absolute;
+    transition: opacity 0.5s ease-in-out;
+    -webkit-transition: opacity 0.5s ease-in-out;
+    -moz-transition: opacity 0.5s ease-in-out;
+    &:nth-of-type(2) {
       opacity: 0;
-      transition: all 0.5s ease-in-out;
-      -webkit-transition: all 0.5s ease-in-out;
-      -moz-transition: all 0.5s ease-in-out;
-      p {
-        margin-top: 12px auto;
-        font-weight: 300;
-        font-size: 16px;
-      }
-      a,
-      span {
-        color: #fff;
-        text-decoration: none;
-        letter-spacing: 1px;
-        cursor: pointer;
-        &:hover {
-          text-decoration: underline;
-        }
-      }
-      &:hover {
+      &:hover ~ div {
         opacity: 1;
         top: 150px;
       }
     }
-    &.highlighted {
-      z-index: 4;
-      .img-portfolio[class*="img-2-"] {
-        opacity: 1;
-        &:hover ~ .project-link {
-          opacity: 0;
-          top: 235px;
-        }
+  }
+  &:hover > img:nth-of-type(2) {
+    opacity: 1;
+  }
+  & > div {
+    width: 200px;
+    background-color: #000;
+    color: #fff;
+    position: absolute;
+    text-align: center;
+    z-index: 2;
+    top: 235px;
+    opacity: 0;
+    transition: all 0.5s ease-in-out;
+    -webkit-transition: all 0.5s ease-in-out;
+    -moz-transition: all 0.5s ease-in-out;
+    p {
+      margin-top: 12px auto;
+      font-weight: 300;
+      font-size: 16px;
+    }
+    a,
+    span {
+      color: #fff;
+      text-decoration: none;
+      letter-spacing: 1px;
+      cursor: pointer;
+      &:hover {
+        text-decoration: underline;
+      }
+    }
+    &:hover {
+      opacity: 1;
+      top: 150px;
+    }
+  }
+  &.highlighted {
+    z-index: 4;
+    img:nth-of-type(2) {
+      opacity: 1;
+      &:hover ~ div {
+        opacity: 0;
+        top: 235px;
       }
     }
   }
+}
 </style>
